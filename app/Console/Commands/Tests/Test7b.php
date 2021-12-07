b31f906748aeaf204a9864e07ce13fd9f4ebd813
Jeppe Knockaert <jeppe.knockaert@gmail.com>
2021-12-07 06:24:10 +0100

    Day 7

--- app/Console/Commands/Tests/Test7b.php
+++ app/Console/Commands/Tests/Test7b.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands\Tests;
 
 use function explode;
 
-class Test7a implements Test
+class Test7b implements Test
 {
     public function getResult(array $inputs): int
     {
@@ -12,10 +12,21 @@ class Test7a implements Test
         $maxPos = $positions->max();
         $smallestSum = -1;
         for ($i = 0; $i < $maxPos; $i++) {
-            $sum = $positions->sum(fn ($pos) => abs($pos-$i));
+            $sum = $positions->sum(fn ($pos) => $this->calculateCost($pos,$i));
             $smallestSum = $smallestSum !== -1 ? min($smallestSum, $sum) : $sum;
         }
 
         return $smallestSum;
+    }
+
+    private function calculateCost(int $position, int $destination): int
+    {
+        $cost = 0;
+        $distance = abs($position - $destination);
+        for ($i = 1; $i <= $distance; $i++) {
+            $cost += $i;
+        }
+
+        return $cost;
     }
 }
