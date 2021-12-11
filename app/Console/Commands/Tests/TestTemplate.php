a93370ae902297709869a15581fb9a1f98db31bd
Jeppe Knockaert <jeppe.knockaert@gmail.com>
2021-12-11 06:23:46 +0100

    Expand test template

--- app/Console/Commands/Tests/TestTemplate.php
+++ app/Console/Commands/Tests/TestTemplate.php
@@ -2,11 +2,29 @@
 
 namespace App\Console\Commands\Tests;
 
+use function array_map;
+use function count;
+use function str_split;
+
 class TestTemplate implements Test
 {
+    private array $inputMatrix;
+    private int $rows;
+    private int $cols;
+
     public function getResult(array $inputs): int
     {
         $output = 0;
+
+        $this->inputMatrix = array_map(fn($input) => str_split($input), $inputs);
+        $this->rows = count($this->inputMatrix);
+        $this->cols = count($this->inputMatrix[0]);
+
+        for ($y = 0; $y < $this->rows; $y++) {
+            for ($x = 0; $x < $this->cols; $x++) {
+                // TODO
+            }
+        }
 
         foreach ($inputs as $input) {
             // TODO
